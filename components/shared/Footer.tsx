0ef96e72f027abb3b7fd5b2fd0641fd71adfeca5
Sidney Lima <sidneylimadev@gmail.com>
2025-01-15 13:54:42 -0300

    feat: footer is ready with date update with script.

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="items-center wrapper flex flex-col justify-center gap-4 p-5 md:justify-between md:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="Ticket Logo"
+            width={144}
+            height={36}
+          />
+        </Link>
+        <p className="text-sm text-muted-foreground">
+          &copy; {new Date().getFullYear()} Ticket. Todos os direitos
+          reservados.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
